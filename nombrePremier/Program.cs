d120dab94bd80432ed8764995e26a5648255f353
nathalie <nata3@hotmail.fr>
2016-11-13 19:52:44 +0100

    Tp Nombres Premiers Asyncrhone

---
+++ nombrePremier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace nombrePremier
+{
+    class Program
+    {
+        private delegate int monDelegue(int x); 
+        private static bool estPremier(int nb)
+        {
+            for(int i = 2; i<=(int)Math.Sqrt(nb); i++)
+            {
+                if(nb % i == 0)
+                {
+                    return false; 
+                }
+            }
+            return true;
+        } 
+
+        private static int ComptePremier(int max)
+        {
+            int resultat = 0;
+            for (int i = 2; i <= max; i++)
+            {
+                if (estPremier(i))
+                {
+                    resultat = resultat + 1; 
+                }
+            }
+            return resultat;
+        }
+        static void Main(string[] args)
+        {
+            monDelegue calcul = ComptePremier;
+            Console.Write("Saisir la valeur maximal pour le calcul : ");
+            string nombreMax = Console.ReadLine();
+            Console.WriteLine();
+            int maximum = Int32.Parse(nombreMax);
+            IAsyncResult asyncResult = calcul.BeginInvoke(maximum, new AsyncCallback(Complete), calcul);
+            
+            Console.WriteLine("Appuyer sur entrée pour arrêter avant la fin du calcul");
+            Console.ReadLine(); 
+        }
+
+        static void Complete(IAsyncResult result)
+        {
+            monDelegue d = (monDelegue)result.AsyncState;
+            Console.WriteLine("Resultat :" + d.EndInvoke(result));
+        }
+    }
+}
